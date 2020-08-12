46f5ef026f86f622d892fc30236e3adeb96a27e0
daichi-iwamoto <da1_0410@ezweb.ne.jp>
2020-08-12 16:40:52 +0900

    add press  img

---
+++ imagemin.js
@@ -0,0 +1,22 @@
+const imagemin = require('imagemin-keep-folder');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminSvgo = require('imagemin-svgo');
+
+imagemin([process.argv[2]], {
+  plugins: [
+    imageminMozjpeg({ quality: 80 }),
+    imageminPngquant(),
+    imageminGifsicle(),
+    imageminSvgo()
+  ],
+  replaceOutputDir: output => {
+    return (
+      console.log(process.argv[2]),
+      output.replace(/img\//, '../dist/img/')
+    )
+  }
+}).then(() => {
+  console.log('Images optimized');
+});
